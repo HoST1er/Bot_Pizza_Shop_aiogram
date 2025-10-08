9e2ff2f13821c1cf16d7af1fe49ff6543b4f7f11
Andy <92803779+HoST1er@users.noreply.github.com>
2025-10-08 16:15:06 +0300

    Update create_bot.py

--- create_bot.py
+++ create_bot.py
@@ -1,6 +1,6 @@
 from aiogram import Bot, Dispatcher
 import os
 
-bot = Bot(token='7373941375:AAH0TSQtktgZDOuEoiQDzeaJhhWFf_lHuQY')
+bot = Bot(token='')
 #bot = Bot(token=os.getenv('TOKEN'))
 dp = Dispatcher()
